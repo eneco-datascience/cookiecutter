de3e711ba420307ea94ee7377e3ee8fc7ff7158f
Huib Keemink <huib.keemink@eneco.com>
2017-05-10 19:57:04 +0200

    updated setup.py to use correct name

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="{{ cookiecutter.python_module_name }}",
+    name="{{ cookiecutter.repo_name }}",
     version="1.0.0",
     packages=find_packages(exclude=['data', 'figures', 'output', 'notebooks', 'tests'])
 )
