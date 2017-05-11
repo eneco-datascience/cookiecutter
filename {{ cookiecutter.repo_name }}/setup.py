96f58b281d0ff1d577931ee9ba9bf271fc2d016d
Huib Keemink <huib.keemink@eneco.com>
2017-05-11 10:06:32 +0200

    updated setup.py to include the source dir, instead of excluding the rest

--- {{ cookiecutter.repo_name }}/setup.py
+++ {{ cookiecutter.repo_name }}/setup.py
@@ -3,5 +3,5 @@ from setuptools import setup, find_packages
 setup(
     name="{{ cookiecutter.repo_name }}",
     version="1.0.0",
-    packages=find_packages(exclude=['data', 'figures', 'output', 'notebooks', 'tests'])
+    packages=find_packages(include=['{{ cookiecutter.repo_name }}',])
 )
